31f184703dea047eee9b017a1632377d7baab56a
yunni <yunnibbd@163.com>
2026-01-18 22:50:52 +0800

    add cocos ui ligic

---
+++ UIProject/assets/Scripts/global.ts
@@ -0,0 +1,21 @@
+import HUDUI from "./hudui";
+
+(function(){
+    // @ts-ignore
+    window.callGlobal = function(...args) {
+        if (args.length == 0) {
+            return;
+        }
+
+        const cmd = args[0];
+        switch(cmd) {
+            case "setMaxAmmo":
+                HUDUI.Instance.setMaxAmmo(args[1]);
+                break;
+            case "updateAmmo":
+                HUDUI.Instance.updateAmmo(args[1]);
+                break;
+        }
+    }
+
+})();
--- UIProject/assets/Scripts/hudui.ts
+++ UIProject/assets/Scripts/hudui.ts
@@ -1,7 +1,12 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class HUDUI extends cc.Component {
+
+    public static Instance:HUDUI = null;
+
+    // 最大弹药数量
+    private m_maxAmmo = 0;
 
     @property(cc.Sprite)
     private hudImg:cc.Sprite = null;
@@ -9,7 +14,25 @@ export default class NewClass extends cc.Component {
     @property(cc.Label)
     private hudLabel:cc.Label = null;
 
+    protected onLoad(): void {
+        if (HUDUI.Instance == null) {
+            HUDUI.Instance = this;
+        }
+    }
+
     protected start(): void {
         this.hudImg.fillRange = 1;
+    }
+
+    public setMaxAmmo(num:number) {
+        this.m_maxAmmo = num;
+    }
+
+    public updateAmmo(num:number) {
+        const usedAmmo = this.m_maxAmmo - num;
+        const newAmmoCount = num;
+        
+        this.hudImg.fillRange = newAmmoCount / this.m_maxAmmo;
+        this.hudLabel.string = `剩余弹药: ${newAmmoCount}/${this.m_maxAmmo}`;
     }
 }
